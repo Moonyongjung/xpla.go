9da6269a6aeaaad1dab62d7db47aef05e109fc61
Moonyongjung <jungmy0913@naver.com>
2023-08-29 11:04:18 +0900

    test: integrate test files

--- client/xplaclient_test.go
+++ client/xplaclient_test.go
@@ -2,17 +2,30 @@ package client_test
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 	"testing"
 
 	"github.com/Moonyongjung/xpla.go/client"
+	"github.com/Moonyongjung/xpla.go/client/xplago_helper"
 	mbank "github.com/Moonyongjung/xpla.go/core/bank"
 	"github.com/Moonyongjung/xpla.go/types"
 	"github.com/Moonyongjung/xpla.go/util"
 	"github.com/Moonyongjung/xpla.go/util/testutil"
+	"github.com/Moonyongjung/xpla.go/util/testutil/network"
+	"github.com/ethereum/go-ethereum/common"
+	ethermint "github.com/evmos/ethermint/types"
+	evmtypes "github.com/evmos/ethermint/x/evm/types"
 
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktestutil "github.com/cosmos/cosmos-sdk/x/bank/client/testutil"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 var (
@@ -94,5 +107,122 @@ func TestNewXplaClient(t *testing.T) {
 	assert.Equal(t, mbank.BankTotalMsgType, xplac.GetMsgType())
 	assert.Equal(t, mbank.BankTotalMsgType, xplac.GetMsgType())
 	assert.Equal(t, totalMsg, xplac.GetMsg())
+}
 
+var (
+	validatorNumberForBroadcast = 2
+	testSendAmount              = "1000"
+)
+
+type ClientTestSuite struct {
+	suite.Suite
+
+	xplac      *client.XplaClient
+	apis       []string
+	accounts   []simtypes.Account
+	testTxHash string
+
+	cfg     network.Config
+	network *network.Network
+}
+
+func NewClientTestSuite(cfg network.Config) *ClientTestSuite {
+	return &ClientTestSuite{cfg: cfg}
+}
+
+func (s *ClientTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
+
+	src := rand.NewSource(1)
+	r := rand.New(src)
+	s.accounts = testutil.RandomAccounts(r, 2)
+	s.testTxHash = "B6BBBB649F19E8970EF274C0083FE945FD38AD8C524D68BB3FE3A20D72DF03C4"
+
+	balanceBigInt, err := util.FromStringToBigInt("1000000000000000000000000000")
+	s.Require().NoError(err)
+
+	genesisState := s.cfg.GenesisState
+
+	// add genesis account
+	var authGenesis authtypes.GenesisState
+	s.Require().NoError(s.cfg.Codec.UnmarshalJSON(genesisState[authtypes.ModuleName], &authGenesis))
+
+	var genAccounts []authtypes.GenesisAccount
+
+	genAccounts = append(genAccounts, &ethermint.EthAccount{
+		BaseAccount: authtypes.NewBaseAccount(s.accounts[0].Address, nil, 0, 0),
+		CodeHash:    common.BytesToHash(evmtypes.EmptyCodeHash).Hex(),
+	})
+	genAccounts = append(genAccounts, &ethermint.EthAccount{
+		BaseAccount: authtypes.NewBaseAccount(s.accounts[1].Address, nil, 0, 0),
+		CodeHash:    common.BytesToHash(evmtypes.EmptyCodeHash).Hex(),
+	})
+
+	accounts, err := authtypes.PackAccounts(genAccounts)
+	s.Require().NoError(err)
+
+	authGenesis.Accounts = accounts
+
+	authGenesisBz, err := s.cfg.Codec.MarshalJSON(&authGenesis)
+	s.Require().NoError(err)
+	genesisState[authtypes.ModuleName] = authGenesisBz
+
+	// add balances
+	var bankGenesis banktypes.GenesisState
+	s.Require().NoError(s.cfg.Codec.UnmarshalJSON(genesisState[banktypes.ModuleName], &bankGenesis))
+
+	bankGenesis.Balances = []banktypes.Balance{
+		{
+			Address: s.accounts[0].Address.String(),
+			Coins: sdk.Coins{
+				sdk.NewCoin(types.XplaDenom, sdk.NewIntFromBigInt(balanceBigInt)),
+			},
+		},
+		{
+			Address: s.accounts[1].Address.String(),
+			Coins: sdk.Coins{
+				sdk.NewCoin(types.XplaDenom, sdk.NewIntFromBigInt(balanceBigInt)),
+			},
+		},
+	}
+
+	bankGenesisBz, err := s.cfg.Codec.MarshalJSON(&bankGenesis)
+	s.Require().NoError(err)
+	genesisState[banktypes.ModuleName] = bankGenesisBz
+
+	s.cfg.GenesisState = genesisState
+	s.network = network.New(s.T(), s.cfg)
+	s.Require().NoError(s.network.WaitForNextBlock())
+
+	val := s.network.Validators[0]
+	newAddr := s.accounts[0].Address
+
+	_, err = banktestutil.MsgSendExec(
+		val.ClientCtx,
+		val.Address,
+		newAddr,
+		sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(200))), fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+		fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+		fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewCoin(s.cfg.BondDenom, sdk.NewInt(10))).String()),
+	)
+	s.Require().NoError(err)
+	s.Require().NoError(s.network.WaitForNextBlock())
+
+	s.xplac = xplago_helper.NewTestXplaClient()
+
+	s.apis = []string{
+		s.network.Validators[0].APIAddress,
+		s.network.Validators[0].AppConfig.GRPC.Address,
+	}
+}
+
+func (s *ClientTestSuite) TearDownSuite() {
+	s.T().Log("tearing down integration test suite")
+	s.network.Cleanup()
+}
+
+func TestClientTestSuite(t *testing.T) {
+	cfg := network.DefaultConfig()
+	cfg.NumValidators = validatorNumberForBroadcast
+	suite.Run(t, NewClientTestSuite(cfg))
 }
